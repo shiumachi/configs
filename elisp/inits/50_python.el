4f88b20ef8b8aaf8fcaec11eb99f6c2c3a6c3d16
Sho Shimauchi <sho.shimauchi@gmail.com>
2011-01-12 07:41:22 +0900

    ignore successive TAB key in python-mode

--- elisp/inits/50_python.el
+++ elisp/inits/50_python.el
@@ -6,3 +6,16 @@
 (setq interpreter-mode-alist (cons '("python" . python-mode) interpreter-mode-alist))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 
+;; http://d.hatena.ne.jp/bellbind/20100723/1279905842
+(add-hook 'python-mode-hook
+          '(lambda()
+             (defun my-indent-line (&optional arg)
+               "modeless indent for python indentation"
+               (interactive "P")
+               (let ((old-this-command this-command))
+                 (setq this-command t)
+                 (py-indent-line arg)
+                 (setq this-command old-this-command)
+                 ))
+             (setq indent-line-function 'my-indent-line)
+             ))
